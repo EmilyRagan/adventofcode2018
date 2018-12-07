67c77b32210d0c33fc7cad447f88e35477b9e2d0
Emily Ragan <emily.lynn.ragan@gmail.com>
2018-12-07 18:16:51 -0500

    part 2 day 2

--- day2.js
+++ day2.js
@@ -27,4 +27,30 @@ fs.readFile('./day2_input.txt', (err, data) => {
 		return val;
 	});
 	console.log(twos * threes);
+	// array iterator
+	for (var i = 0; i < strings.length - 1; i++) {
+		var strI = strings[i];
+		// second array iterator
+		var j = 0;
+		for (j = i + 1; j < strings.length; j++) {
+			var strJ = strings[j];
+			var diffIndex = -1;
+			// letter in string iterator
+			for (var m = 0; m < strI.length; m++) {
+				// compare letters at each index
+				// if only one letter different
+					// remove that letter
+					// return the rest of the letters
+				if (strI.charAt(m) !== strJ.charAt(m) && diffIndex === -1) {
+					diffIndex = m;
+				} else if (strI.charAt(m) !== strJ.charAt(m) && diffIndex !== -1) {
+					diffIndex = -1;
+					break;
+				}
+			}
+			if (diffIndex !== -1) {
+				console.log(strI.substr(0, diffIndex) + strI.substr(diffIndex + 1));
+			}
+		}
+	}
 });
